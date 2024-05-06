713b0bcfda1592995c7fc0a33745c6e3f8137299
OmidSargazi <om.sargazi@gmail.com>
2024-05-06 21:05:26 +0330

    delete prime number from an array

--- programming/excersise.js
+++ programming/excersise.js
@@ -10,16 +10,46 @@ function rotateRight() {
   A[0] = temp;
   console.log(A);
 }
-rotateRight();
+// rotateRight();
 
 function rotateLeft() {
   let A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
   let n = A.length;
-  let temp = A[0];
-  for (let i = 1; i < n; i++) {
-    A[i - 1] = A[i];
+  for (let x = 0; x < n; x++) {
+    let temp = A[0];
+    for (let i = 1; i < n; i++) {
+      A[i - 1] = A[i];
+    }
+    A[n - 1] = temp;
   }
-  A[n - 1] = temp;
   console.log(A);
 }
-rotateLeft();
+// rotateLeft();
+
+function deletePrimeNumber() {
+  let A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+  let n = A.length;
+  let cA = n;
+  let prime = [];
+  let cPrime = 0;
+  let iA = 0;
+  while (iA < cA) {
+    let flag = 0;
+    for (let i = 1; i <= A[iA]; i++) {
+      if (A[iA] % i == 0) {
+        flag++;
+      }
+    }
+    if (flag != 2) {
+      iA++;
+    } else {
+      prime[cPrime++] = A[iA];
+      for (let x = iA + 1; x < cA; x++) {
+        A[x - 1] = A[x];
+      }
+      cA--;
+    }
+  }
+  console.log(A, "A after");
+}
+deletePrimeNumber();
