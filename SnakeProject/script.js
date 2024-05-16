d24aadf1e90e476343a62006e5a4731654dcbeef
OmidSargazi <om.sargazi@gmail.com>
2024-05-16 21:19:14 +0330

    created food item for snake

--- SnakeProject/script.js
+++ SnakeProject/script.js
@@ -6,11 +6,13 @@ let snake;
 let direction;
 let startStopHandler;
 let headX, headY;
+let food, foodX, foodY;
 function initialize() {
   body.innerHTML = "";
   createImageBoard();
   createSnake(5);
   drawSnake();
+  insertFood();
   direction = "R";
   startStopHandler = setInterval(move, 100);
 }
@@ -24,8 +26,8 @@ function createImageBoard() {
 
     w = parseInt((window.innerWidth * 0.75) / 10) * 10;
     h = parseInt((window.innerHeight - 10) / 10) * 10;
-    grideHeight = h / 10;
     gridWidth = w / 10;
+    grideHeight = h / 10;
 
     gameBoard.style.width = w + "px";
     gameBoard.style.height = h + "px";
@@ -62,31 +64,46 @@ function drawSnake() {
   gameBoard.innerHTML = "";
   for (let i = 0; i < snake.length; i++) {
     let p = document.createElement("span");
+
     p.style.display = "block";
     p.style.width = "10px";
     p.style.height = "10px";
     p.style.borderRadius = "5px";
     p.style.background = "red";
     p.style.boxShadow = "0 0 2px 0 black";
-    p.style.gridColumn = snake[i][1];
-    p.style.gridRow = snake[i][0];
+    p.style.gridColumn = snake[i][0];
+    p.style.gridRow = snake[i][1];
     gameBoard.appendChild(p);
   }
+
+  let p = document.createElement("span");
+  p.style.display = "block";
+  p.style.width = "10px";
+  p.style.height = "10px";
+  p.style.background = "green";
+  p.style.boxShadow = "0 0 2px 0 black";
+  p.style.gridColumn = foodX;
+  p.style.gridRow = foodY;
+  gameBoard.appendChild(p);
 }
 
 function move() {
   switch (direction) {
     case "R":
       headX++;
+      if (headX > gridWidth) headX = 1;
       break;
     case "L":
       headX--;
+      if (headX < 1) headX = gridWidth;
       break;
     case "U":
       headY--;
+      if (headY < 1) headY = grideHeight;
       break;
     case "D":
       headY++;
+      if (headY > grideHeight) headY = 1;
       break;
   }
   for (let i = 0; i < snake.length - 1; i++) {
@@ -115,5 +132,25 @@ window.addEventListener("keydown", function (e) {
       break;
   }
 });
+
+function insertFood() {
+  let fx = false;
+  let x = 0,
+    y = 0;
+  while (!fx) {
+    fx = true;
+    x = parseInt(Math.random() * (gridWidth - 1) + 1);
+    y = parseInt(Math.random() * (grideHeight - 1) + 1);
+
+    console.log("inserting food");
+    for (let i = 0; i < snake.length; i++) {
+      if (snake[i][0] == x && snake[i][1] == y) {
+        fx = false;
+      }
+    }
+  }
+  foodX = x;
+  foodY = y;
+}
 
 initialize();
