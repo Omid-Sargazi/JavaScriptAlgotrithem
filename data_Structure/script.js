461495956a933faa780a37ff392d87f070997990
OmidSargazi <om.sargazi@gmail.com>
2024-05-22 13:54:50 +0330

    big number and big factorial

--- data_Structure/script.js
+++ data_Structure/script.js
@@ -3,6 +3,7 @@ function bigFactorial() {
   let cA = 6;
   let n = 62;
   let q = 0;
+
   for (let i = 0; i < cA; i++) {
     let t = A[i] * n + q;
     A[i] = t % 10;
@@ -17,4 +18,29 @@ function bigFactorial() {
     console.log(A[i]);
   }
 }
-bigFactorial();
+
+function factorial() {
+  let A = new Array(10000).fill(0);
+  A[0] = 1;
+  let cA = 1;
+
+  for (let i = 1; i <= 10000; i++) {
+    let q = 0;
+    for (let x = 0; x < cA; x++) {
+      let t = A[x] * i + q;
+      A[x] = t % 10;
+      q = Math.floor(t / 10);
+    }
+    while (q > 0) {
+      A[cA++] = q % 10;
+      q = Math.floor(q / 10);
+    }
+  }
+  let result = "";
+  for (let i = cA - 1; i >= 0; i--) {
+    result += A[i];
+  }
+  console.log(result);
+}
+
+factorial();
