fcdcf414488085d2f17ee1f65c3d28da4f45978e
OmidSargazi <om.sargazi@gmail.com>
2024-05-25 18:55:58 +0330

    Trim a string from left

--- programming/strToInt.js
+++ programming/strToInt.js
@@ -7,4 +7,21 @@ function StrToInt() {
   console.log(p);
 }
 
-StrToInt();
+// StrToInt();
+
+function trimString() {
+  let S = "       omid          ";
+  let i1 = 0;
+  let i2 = S.length - 1;
+
+  while (i1 < S.length && S[i1] === " ") i1++;
+  while (i2 > i1 && S[i2] === " ") i2--;
+  let R = "";
+
+  for (let x = i1; x <= i2; x++) {
+    R = R + S[x];
+  }
+
+  console.log(R, R.length);
+}
+trimString();
